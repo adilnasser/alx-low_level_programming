ce80176f981e67c8f319da47e377d06482917af8
Nasser <nasserrabahadil@gmail.com>
2023-02-21 18:24:05 -0800

    Tsk1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*print_alphabet prints the alphabet, in lowercase, followed by a new line.
+*print_alphabet prints the alphabet, in lowercase
 *Return:void 
 */
 void print_alphabet(void)
