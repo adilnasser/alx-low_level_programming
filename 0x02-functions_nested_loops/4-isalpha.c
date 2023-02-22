156ad883813c92feeabcbf92bdfc6ffe31bfec95
Nasser <nasserrabahadil@gmail.com>
2023-02-22 13:32:50 -0800

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,10 +5,11 @@
  * @i: declared character
  * Return: Always 1 if aplha else return 0.
  */
+
 int _isalpha(int i)
 {
-  if ((i >= 65 && i <= 95) || (i >= 97 && i <= 122))
-      return (1);
-   else
-  return (0);
+if ((i >= 65 && i <= 95) || (i >= 97 && i <= 122))
+return (1);
+else
+return (0);
 }
