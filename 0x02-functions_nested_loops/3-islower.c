bb3daee83136934e804f5495f46c245de32b4b5f
Nasser <nasserrabahadil@gmail.com>
2023-02-22 13:03:15 -0800

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,10 +3,11 @@
 /**
  *int _islower -function that checks for lowercase character
  */
+
 int _islower(int c)
 {
-  if (c >= 'a' && c <= 'z')
-    return (1);
-  else
-    return (0);
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
 }
