eba64b5954be7753cc950751cef614f6d082bc10
Nasser <nasserrabahadil@gmail.com>
2023-02-21 18:18:24 -0800

    Tsk1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,8 +2,7 @@
 
 /**
 *print_alphabet prints the alphabet, in lowercase, followed by a new line.
-*Return: On success 1.
- On error, -1 is returned, and errno is set appropriately.
+*Return:void 
 */
 
 void print_alphabet(void)
