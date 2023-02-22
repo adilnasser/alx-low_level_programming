7832f20e183d1f38ef5aeee1897d811804b5fe11
Nasser <nasserrabahadil@gmail.com>
2023-02-22 13:43:04 -0800

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_sign - check the code
+ * @i: declared integer
+ * Return: 1 if is greater than zero
+ * Return: -1 if is less than zero
+ * Return: 0 if is zero
+ */
+
+int print_sign(int n)
+{
+if (i > 0)
+return (1);
+else if (i < 0)
+return (-1);
+else
+return (0);
+}
