d5ea2c31d3d0dfeec41ecf102ee320f797859bd7
Nasser <nasserrabahadil@gmail.com>
2023-02-23 18:35:07 -0800

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,8 @@
 
 /**
  * main - prints the numbers from 1 to 100, but for multiples of three
- *        Fizz instead of the number and for the multiples of five print Buzz.
- *        For numbers which are multiples of both three and five print FizzBuzz.
+ * Fizz instead of the number and for the multiples of five print Buzz.
+ * For numbers which are multiples of both three and five print FizzBuzz.
  *
  * Return: Always 0.
  */
