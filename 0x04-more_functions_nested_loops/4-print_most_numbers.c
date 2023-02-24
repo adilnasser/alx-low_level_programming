d6e0b893d5060731fb2b30bdfea0f707bc2920bf
Nasser <nasserrabahadil@gmail.com>
2023-02-23 16:14:08 -0800

    Task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print _most_numbers - prints the numbers, from 0 to 9
+ * print_most_numbers - prints the numbers, from 0 to 9
  * Do not print 2 and 4
  * Return: no return.
  */
