cf9be7a93271095cffe76a74df577c88090568c9
Nasser <nasserrabahadil@gmail.com>
2023-02-27 15:24:20 -0800

    Task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98- takes a pointer to an int and updates the value it points to to 98
+ * reset_to_98- takes a pointer and updates the value it points to to 98
  * *n :pointer
  * Return: no return.
  */
