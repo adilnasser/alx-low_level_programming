15e74ae8a3fc7b5cabc9b9e4606edd086c98b64b
Nasser <nasserrabahadil@gmail.com>
2023-02-27 18:25:42 -0800

    Task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2- nction that prints every other character of a string
+ * @str :string
+ * Return: No Return;
+ */
+
+
+void puts2(char *str)
+{
+	int length_Of_String = 0;
+
+	int i = 0;
+
+	while (str[length_Of_String] != '\0')
+	{
+		for (i = 0 ; i <= length_Of_String  ; i++)
+		{
+			if (i % 2 == 0)
+				_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+
+}
