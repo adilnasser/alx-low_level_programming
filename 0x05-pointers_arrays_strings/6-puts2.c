f601e3b49ff1529e6b4d5ad61b256fd9b2bfe2a6
Nasser <nasserrabahadil@gmail.com>
2023-02-27 18:38:59 -0800

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * puts2- nction that prints every other character of a string
- * @str :string
- * Return: No Return;
- */
-
-
+* puts2 - prints one char out of 2 of a string
+* followed by a new line
+* @str: string to print the chars from
+*/
 void puts2(char *str)
 {
-	int length_Of_String = 0;
+int len, i;
 
-	int i = 0;
+len = 0;
 
-	while (str[length_Of_String] != '\0')
-	{
-		for (i = 0 ; i < length_Of_String ; i += 2)
-		{
-				_putchar(str[i]);
-		}
-	}
-	_putchar('\n');
-
+while (str[len] != '\0')
+{
+len++;
 }
+
+for (i = 0; i < len; i += 2)
+       {
+       _putchar(str[i]);
+       }
+
+       _putchar('\n');
+       }
