bb607590280572a3d9f33d1177fbd791ba48d2f2
Nasser <nasserrabahadil@gmail.com>
2023-03-01 20:39:06 -0800

    Task0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strcat - appends the src string to the dest string
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ *
+ * Return: pointer to the resulting string dest, which is the concatenation of
+ *         dest and src strings.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int lenghtdest = 0, lenghtsrc = 0;
+
+	int i = 0, j = 0;
+
+	while (dest[lenghtdest] != '\0')
+	{
+		lenghtdest++;
+	}
+
+	while (src[lenghtsrc] != '\0')
+	{
+		lenghtsrc++;
+	}
+
+	for (i = lenghtdest + 1; i <= lenghtdest + 1 ; i++)
+	{
+		while (j < lenghtsrc)
+		{
+			*(dest + i) = *(src + j);
+			j++;
+		}
+	}
+	return (dest);
+}
