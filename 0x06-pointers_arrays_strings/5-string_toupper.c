0458ab546f8666e896b5dde0755306332dc842e7
Nasser <nasserrabahadil@gmail.com>
2023-03-02 18:14:20 -0800

    Task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts all lowercase letters of a string to uppercase
+ * @str: the string to modify
+ *
+ * Return: a pointer to the modified string
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr)
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr -= 'a' - 'A';
+		}
+		ptr++;
+	}
+	return (str);
+}
