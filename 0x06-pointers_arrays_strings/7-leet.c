b6c823be71a851acd8a87e148ac1a61b882cd073
Nasser <nasserrabahadil@gmail.com>
2023-03-02 18:26:57 -0800

    Task7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: input string
+ *
+ * Return: pointer to resulting string
+ */
+char *leet(char *s)
+{
+	char *ptr = s;
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	while (*ptr)
+	{
+		i = 0;
+		while (letters[i])
+		{
+			if (*ptr == letters[i])
+			{
+				*ptr = numbers[i];
+				break;
+			}
+			i++;
+		}
+		ptr++;
+	}
+
+	return (s);
+}
