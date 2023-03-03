862414ac917133331098add0a55e2e785e4091d2
Nasser <nasserrabahadil@gmail.com>
2023-03-02 17:21:05 -0800

    Task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - appends the src string to the dest string
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ * @n: number of bytes from @src
+ * Return: pointer to the resulting string dest, which is the concatenation of
+ *         dest and src strings.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	int j = 0;
+
+	while (*(src + j) != '\0'  && j < n)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+
+	dest[j] = '\0';
+
+	return (dest);
+}
