ccf0d7ce6be6e3e046332093a3177997ac315d69
Nasser <nasserrabahadil@gmail.com>
2023-03-02 18:42:29 -0800

    Task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,22 +6,25 @@
 *
 * Return: pointer to the encoded string
 */
+
 char *leet(char *str)
 {
 int i, j;
+
 char leet[] = "aAeEoOtTlL4433007711";
 
 for (i = 0; str[i] != '\0'; i++)
        {
-       for (j = 0; leet[j] != '\0'; j += 2)
-	      {
-	      if (str[i] == leet[j])
-	      {
-	      str[i] = leet[j + 1];
-	      break;
-	      }
-	      }
-	      }
 
-	      return (str);
+	       for (j = 0; leet[j] != '\0'; j += 2)
+	       {
+		       if (str[i] == leet[j])
+		       {
+			       str[i] = leet[j + 1];
+			       break;
+		       }
 	      }
+	}
+
+      return (str);
+}
