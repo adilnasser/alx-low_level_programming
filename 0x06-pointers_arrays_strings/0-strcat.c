51821230bfb778bd19c0602cfdc73981aa20b5dc
Nasser <nasserrabahadil@gmail.com>
2023-03-02 16:05:22 -0800

    Task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,42 +1,36 @@
 #include "main.h"
 
 /**
- * _strcat - appends the src string to the dest string
- * @dest: pointer to the destination string
- * @src: pointer to the source string
- *
- * Return: pointer to the resulting string dest, which is the concatenation of
- *         dest and src strings.
- */
+* _strcat - appends the src string to the dest string
+* @dest: pointer to the destination string
+* @src: pointer to the source string
+*
+* Return: pointer to the resulting string dest, which is the concatenation of
+*         dest and src strings.
+*/
 
 char *_strcat(char *dest, char *src)
 {
 
-	int lenghtdest = 0, lenghtsrc = 0;
+int lenghtdest = 0, lenghtsrc = 0;
 
-	int i = 0, j = 0;
+int j = 0;
 
-	while (dest[lenghtdest] != '\0')
-	{
-		lenghtdest++;
-	}
-
-	while (src[lenghtsrc] != '\0')
-	{
-			    lenghtsrc++;
-	}
-
-	for (i = lenghtdest; i < lenghtdest + lenghtsrc; i++)
-	{
-		while (j < lenghtsrc)
-		{
-			*(dest + i) = *(src + j);
-			j++;
-		}
-	}
+while (dest[lenghtdest] != '\0')
+lenghtdest++;
 
 
-	*(dest + lenghtdest + lenghtsrc) = '\0';
+while (src[lenghtsrc] != '\0')
+lenghtsrc++;
 
-	return (dest);
+while (*(src + j) != '\0')
+{
+*(dest + lenghtdest) = *(src + j);
+lenghtdest++;
+j++;
+}
+
+*(dest + lenghtdest) = '\0';
+
+return (dest);
 }
