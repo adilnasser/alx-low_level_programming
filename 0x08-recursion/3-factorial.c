6e0bb4289b90ad13c45c94dcb74fef824a7b10ec
Nasser <nasserrabahadil@gmail.com>
2023-03-08 18:35:49 -0800

    Task3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  factorial - function that returns the length of a string.
+ * @n: given number
+ * Return: factorial of given number
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
