5185ec0a86b5753c86a72d39d4cddb4b025948e1
Nasser <nasserrabahadil@gmail.com>
2023-03-09 16:37:48 -0800

    Task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,12 +8,21 @@
 
 int is_prime_number(int n)
 {
-	if (n != 0)
-	{
-		return (1);
-	}
-	else
+	if (n == 1)
+		return (0);
+
+	else if (n < 0)
 	{
 		return (0);
 	}
+
+	else if (n != 0)
+	{
+		if (n % 2 == 0)
+		return (0);
+		else
+			return (1);
+	}
+
+	return (1);
 }
