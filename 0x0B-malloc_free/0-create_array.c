7822de752a2780ffdbdf7d08b7d76c85f3387dd0
Nasser <nasserrabahadil@gmail.com>
2023-03-14 17:18:41 -0700

    Task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,23 +12,26 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char* array;
+	char *array;
 
 	unsigned int i  = 0;
 
 	if (size == 0)
 		return (NULL);
-
+	if (array == NULL)
+		return (NULL);
 
 	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
+
 
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
 
-	if (array == NULL)
-		return (NULL);
 
 	return (array);
 }
