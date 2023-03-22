7bb5cc2bc973c7939d2a08eb0434b68064719307
Nasser <nasserrabahadil@gmail.com>
2023-03-21 19:50:17 -0700

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,82 +2,70 @@
 #include <stdlib.h>
 
 /**
- * make_change - prints the minimum number of coine for an amount
- *               of money.
- * @cents: The amount of money in cents.
- *
- * Return: The minimum number of coins.
- */
+* main - Prints the minimum number of coins to make change for a given amount
+* of money.
+* @argc: The number of arguments passed to the program.
+* @argv: An array of pointers to the arguments.
+* Return: Always 0.
+*/
 
-int make_change(int cents)
+
+int coin_count(int cents);
+
+int main(int argc, char **argv)
 {
-	int coins = 0;
+int cents, coins;
 
-	while (cents > 0)
-	{
-		if (cents % 25 == 0)
-		{
-			cents -= 25;
-			coins = coins + (cents / 25);
-		}
-
-		else if (cents % 10 == 0)
-		{
-			cents -= 10;
-			coins += (cents / 10);
-		}
-
-		else if (cents % 5 == 0)
-		{
-			cents -= 5;
-			coins += (cents / 5);
-		}
-
-		else if (cents % 2 == 0)
-		{
-		cents -= 2;
-		coins += (cents / 2);
-		}
-		else
-		{
-		cents -= 1;
-		coins++;
-		}
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
 }
 
-return (coins);
+cents = atoi(argv[1]);
+
+if (cents < 0)
+{
+printf("0\n");
+return (0);
+}
+
+coins = coin_count(cents);
+
+printf("%d\n", coins);
+
+return (0);
 }
 
 
 /**
- * main - prints the minimum number of coins to make change for an amount of
- *        money.
- * @argc: The number of arguments.
- * @argv: The array of arguments.
- *
- * Return: 0 (Always).
- */
-int main(int argc, char *argv[])
+* coin_count - Returns the minimum number of coins to make change for a given
+* amount of money.
+*
+* @cents: The amount of money to make change for.
+*
+* Return: The minimum number of coins.
+*/
+
+int coin_count(int cents)
 {
-	int cents, coins;
+int coins = 0;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
+while (cents > 0)
+{
+if (cents >= 25)
+cents -= 25;
+else if (cents >= 10)
+cents -= 10;
+else if (cents >= 5)
+cents -= 5;
+else if (cents >= 2)
+cents -= 2;
+else
+cents -= 1;
 
-	cents = atoi(argv[1]);
+coins++;
+}
 
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	coins = make_change(cents);
-
-	printf("%d\n", coins);
-
-	return (0);
+return (coins);
 }
