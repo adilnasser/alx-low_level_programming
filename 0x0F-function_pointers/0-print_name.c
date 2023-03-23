7ae9135a7dc4fee962cc56d6f987bd43b62afc41
Nasser <nasserrabahadil@gmail.com>
2023-03-22 17:52:52 -0700

    0. What's my name & adding the header file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function pointer to format the name
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
