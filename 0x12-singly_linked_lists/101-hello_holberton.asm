bddbb9bdef3c856445b40803b46b153b73298583
Nasser <nasserrabahadil@gmail.com>
2023-03-30 19:43:20 -0700

    6. Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,15 @@
-	section .data
-	hello db 'Hello, Holberton', 0x0a ; The string to print
-	section .text
-	global main
+SECTION .data
+msg:db "Hello, Holberton", 0
+fmt:db "%s", 10, 0
+
+SECTION .text
+extern printf
+global main
 main:
-	push rbp 		; Save the base pointer
-	mov rbp, rsp 	; Set up the stack frame
-	mov rdi, hello 	; Load the address of the string into the first argument register
-	xor eax, eax 	; Set the return value to zero
-	call printf	; Call the printf function
-	mov rsp, rbp 	; Restore the stack pointer
-	pop rbp	; Restore the base pointer
-	    ret	; Return to the operating system
+mov esi, msg
+mov edi, fmt
+mov eax, 0
+call printf
+
+mov eax, 0
+ret
