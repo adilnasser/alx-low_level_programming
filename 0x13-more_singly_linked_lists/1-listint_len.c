da029aa3e70454554f8ccb1a633efa260344b899
Nasser <nasserrabahadil@gmail.com>
2023-04-04 14:54:22 -0700

    1. List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+
+/**
+ * listint_len - Prints all the elements of a list_t list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
