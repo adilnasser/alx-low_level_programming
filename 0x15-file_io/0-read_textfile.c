587b7b961211640f337918a4104da6485c23ec8c
Nasser <nasserrabahadil@gmail.com>
2023-04-10 17:10:05 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,39 @@
-#include <unistd.h>
-#include <fcntl.h>
+#include <stddef.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: the name of the file to read
- * @letters: the number of letters to read and print
- *
- * Return: the actual number of letters read and printed, or 0 on failure
- */
+* read_textfile - Reads a text file and prints it to POSIX stdout.
+* @filename: A pointer to the name of the file.
+* @letters: The number of letters the  function should read and print.
+*
+* Return: If the function fails or filename is NULL - 0.
+*         O/w - the actual number of bytes the function can read and print.
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char buffer[1024];
-	ssize_t bytes_read, bytes_written, total_bytes = 0;
+ssize_t o, r, w;
+char *buffer;
 
-	if (filename == NULL)
-		return (0);
+if (filename == NULL)
+return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
 
-	while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0)
-	{
-		bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-		{
-			close(fd);
-			return (0);
-		}
-		total_bytes += bytes_written;
-		if ((size_t)total_bytes >= letters)
-			break;
-	}
+o = open(filename, O_RDONLY);
+r = read(o, buffer, letters);
+w = write(STDOUT_FILENO, buffer, r);
 
-	close(fd);
-	return (total_bytes);
+if (o == -1 || r == -1 || w == -1 || w != r)
+{
+free(buffer);
+return (0);
+}
+
+free(buffer);
+close(o);
+
+return (w);
 }
