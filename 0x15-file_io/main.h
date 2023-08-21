f9191462aff9915c753f4bf1f70ae66925476088
nasseradil <nasserrabahadil@gmail.com>
2023-08-21 07:04:12 -0700

    3. cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,11 +5,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int _putchar(char c);
-void error_exit(int code, const char *message, ...);
 
 #endif
